2d5e0d81815e62fbedf16637fd2f72ceed179269
Arthur Schut <aschutgh@gmail.com>
2018-10-02 11:31:15 +0200

    project create properties added

---
+++ CreateProperties/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CreateProperties
+{
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value > 0.0) // value is a keyword!!
+                    length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value > 0.0) // value is a keyword!!
+                    width = value;
+            }
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Length = 10.0;
+            rect.Width = 20.0;
+            double area = rect.GetArea();
+            Console.WriteLine("Area of Rectangle: {0}", area);
+        }
+    }
+}
