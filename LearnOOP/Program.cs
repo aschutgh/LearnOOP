1c2d3fb1a204a52797922d9a61a6f10e3cb33331
Arthur Schut <aschutgh@gmail.com>
2018-10-02 10:41:15 +0200

    Add project files.

---
+++ LearnOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
