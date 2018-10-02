c0f18326c7e124a37b9a6023915d45972ef74c54
Arthur Schut <aschutgh@gmail.com>
2018-10-02 11:44:05 +0200

    CreateStaticMembers example added

---
+++ CreateStaticMembers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CreateStaticMembers
+{
+    class Rectangle
+    {
+        public static string ShapeName
+        {
+            get { return "Rectangle"; }
+        }
+
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double GetArea()
+        {
+            return this.Length * this.Width;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle
+            { Length = 10.0, Width = 20.0 };
+            Console.WriteLine("Shape Name: {0}, Area: {1}",
+                Rectangle.ShapeName,
+                rect.GetArea());
+        }
+    }
+}
